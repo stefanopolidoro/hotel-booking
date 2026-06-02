0310460911d270c4fe6a4c6c6f7f561153a403a5
stefanopolidoro <polidorostefano8@gmail.com>
2026-06-02 11:20:30 +0200

    chore: bootstrap front controller, config e Composer autoloader

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+define('ROOT_PATH', dirname(__DIR__));
+define('APP_PATH', ROOT_PATH . '/app');
+define('SRC_PATH', ROOT_PATH . '/src');
+
+require_once ROOT_PATH . '/config/config.php';
+
+echo 'Fondamenta ok';
